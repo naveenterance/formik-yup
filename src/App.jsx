741bc31388125ba07564275ea88c8fe08b65a03d
naveenterance <naveenterance@gmail.com>
2023-11-13 06:22:10 +0530

    required

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,90 @@
-import React from "react";
-import { useFormik } from "formik";
+import React, { useEffect, useState } from "react";
+import { useFormik, useFormikContext } from "formik";
+import * as Yup from "yup";
 
-const MyForm = () => {
+const SignupForm = () => {
+  const [reqn, setreqn] = useState("");
+  const [reqe, setreqe] = useState("");
   const formik = useFormik({
     initialValues: {
-      field1: "",
-      field2: "",
+      firstName: "",
+      lastName: "",
+      email: "",
     },
+    validationSchema: Yup.object({
+      firstName: Yup.string()
+        .max(15, "Must be 15 characters or less")
+        .required(""),
+      lastName: Yup.string()
+        .max(20, "Must be 20 characters or less")
+        .required(""),
+      email: Yup.string().email("Invalid email address").required(""),
+    }),
     onSubmit: (values) => {
-      console.log(values);
+      alert(JSON.stringify(values, null, 2));
     },
   });
+  useEffect(() => {
+    console.log(`firstname is now ${formik.values.firstName}`);
+    console.log(`lastname is now ${formik.values.lastName}`);
+    if (!formik.values.firstName && formik.values.lastName) {
+      setreqn("-----*First Name is Required");
+      console.log(reqn);
+    } else setreqn("");
+  }, [formik.values.lastName, formik.values.firstName]);
 
-  const handleField1Change = (event) => {
-    formik.handleChange(event);
-    if (!event.target.value) {
-      formik.setFieldValue("field2", "");
-    }
-  };
+  useEffect(() => {
+    console.log(`firstname is now ${formik.values.email}`);
+    console.log(`lastname is now ${formik.values.lastName}`);
+    if (!formik.values.lastName && formik.values.email) {
+      setreqe("-----*Last Name is Required");
+      console.log(reqe);
+    } else setreqe("");
+  }, [formik.values.lastName, formik.values.email]);
 
   return (
     <form onSubmit={formik.handleSubmit}>
+      <label htmlFor="firstName">First Name{reqn}</label>
       <input
+        id="firstName"
+        name="firstName"
         type="text"
-        name="field1"
-        value={formik.values.field1}
-        onChange={handleField1Change}
-      />
-      <input
-        type="text"
-        name="field2"
-        value={formik.values.field2}
-        disabled={!formik.values.field1}
         onChange={formik.handleChange}
+        onBlur={formik.handleBlur}
+        value={formik.values.firstName}
       />
+      {formik.touched.firstName && formik.errors.firstName ? (
+        <div>{formik.errors.firstName}</div>
+      ) : null}
+
+      <label htmlFor="lastName">Last Name{reqe}</label>
+      <input
+        id="lastName"
+        name="lastName"
+        type="text"
+        onChange={formik.handleChange}
+        onBlur={formik.handleBlur}
+        value={formik.values.lastName}
+      />
+      {formik.touched.lastName && formik.errors.lastName ? (
+        <div>{formik.errors.lastName}</div>
+      ) : null}
+
+      <label htmlFor="email">Email Address</label>
+      <input
+        id="email"
+        name="email"
+        type="email"
+        onChange={formik.handleChange}
+        onBlur={formik.handleBlur}
+        value={formik.values.email}
+      />
+      {formik.touched.email && formik.errors.email ? (
+        <div>{formik.errors.email}</div>
+      ) : null}
+
       <button type="submit">Submit</button>
     </form>
   );
 };
-
-export default MyForm;
+export default SignupForm;
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import "./index.css";
+
 import Form from "./Form.jsx";
+import SignupForm from "./App.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Form />
+    <SignupForm />
   </React.StrictMode>
 );
